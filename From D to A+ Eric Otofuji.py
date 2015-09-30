d82f728b5e2c5357acebb80602c6b484325e334f
Otofuji <ericfoa@al.insper.edu.br>
2015-09-30 10:09:37 -0300

    Começando o processo de ocultação
    Começa a base para poder esconder navios, criando funções que criam
    valores aleatórios para colunas e linhas na matriz, além de importar a
    função randint.

--- From D to A+ Eric Otofuji.py
+++ From D to A+ Eric Otofuji.py
@@ -14,4 +14,11 @@ for i in range(5):
 def print_board(board):
     for i in board:
 		print(" ".join(i))
-		
+
+from random import randint
+
+def random_row(board):
+    return randint(0, len(board)-1)
+
+def random_col(board):
+    return randint(0, len(board)-1)
